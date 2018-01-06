4a90f84ffc677be0db5da55d1a89fa95ca76fd08
Laura Weber <Laura Weber>
2018-01-06 19:19:20 +0100

    3eme partie de fingerprint

--- src/fingerprint/Fingerprint.java
+++ src/fingerprint/Fingerprint.java
@@ -1,6 +1,7 @@
 package fingerprint;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -52,29 +53,59 @@ public class Fingerprint {
 	}
 	
 	public static int fingerprint(int p, String fn) {
-	int finger = 0;
-	try {
-		FileInputStream file = new FileInputStream(fn);
-		int n = file.available();
-		int tab[] = new int[n];
-		tab[0] = 1 % p;
-		for (int i=1; i<n; i++) {
-			tab[i] = (tab[i-1] * 256) % p;
-		}
+		int finger = 0;
+		try {
+			FileInputStream file = new FileInputStream(fn);
+			int n = file.available();
+			int tab[] = new int[n];
+			tab[0] = 1 % p;
+			for (int i=1; i<n; i++) {
+				tab[i] = (tab[i-1] * 256) % p;
+			}
 		
-		int octet;
-		while ((octet = file.read()) != -1) {			
-			finger += ((octet % p) * tab[n-1]) % p;
-			n--;				
-		}
-		file.close();
+			int octet;
+			while ((octet = file.read()) != -1) {	
+				finger += ((octet % p) * tab[n-1]) % p;
+				n--;				
+			}
+			file.close();
 		
-	} catch(IOException e) {
-		e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
 	}
 	
 	return finger % p;
-}
+	}
+	
+	public static int fingerprint(int p, String fn, int taille, int debut) {
+		int finger = 0, cmp = 0;
+		try {
+			FileInputStream file = new FileInputStream(fn);
+			int n = taille;
+			int tab[] = new int[n];
+			tab[0] = 1 % p;
+			for (int i=1; i<n; i++) {
+				tab[i] = (tab[i-1] * 256) % p;
+			}
+		
+			int octet;
+			for(int i = 0; i < debut; i++){
+				octet = file.read();
+			}
+			while ((octet = file.read()) != -1 && cmp < taille) {	
+				finger += ((octet % p) * tab[n-1]) % p;
+				n--;		
+				cmp++;
+			}
+			file.close();
+		
+		} catch(IOException e) {
+			e.printStackTrace();
+	}
+	
+	return finger % p;
+	}
+	
 	//Temps d'exec très long attention !!! 
 	public static void testQ5(){
 		int cmp = 0, premier, finger1, finger2;
@@ -108,12 +139,67 @@ public class Fingerprint {
 	
 	public static void testQ7(){
 		System.out.println(fingerprint(17,"test2"));
-		System.out.println(fingerprint(17,"test2"));
-
-
+		System.out.println(fingerprint(59,"test2"));
+		System.out.println(fingerprint(521,"test2"));
+		System.out.println(fingerprint(997,"test2"));
+		System.out.println(fingerprint(2399,"test2"));
+	}
+	
+	public static boolean testContenu(String fn1, String fn2){
+		boolean trouve = false;
+		FileInputStream file1, file2;
+		int n1, n2, premier = nextprime(), finger1, finger2, cmp;
+		try {
+			file1 = new FileInputStream(fn1);
+			file2 = new FileInputStream(fn2);
+			n1 = file1.available();
+			n2 = file2.available();
+			if(n1 > n2){
+				cmp = 0;
+				finger2 = fingerprint(premier,fn2);
+				while(cmp + n2 < n1 + 1 && !trouve){
+					finger1 = fingerprint(premier, fn1, n2, cmp);
+					if(finger1 == finger2){
+						trouve = true;
+					}
+					cmp = cmp + 1;
+				}
+			}else{
+				cmp = 0;
+				finger1 = fingerprint(premier,fn1);
+				while(cmp + n1 < n2 + 1 && !trouve){
+					finger2 = fingerprint(premier, fn2, n1, cmp);
+					if(finger1 == finger2){
+						trouve = true;
+					}
+					cmp = cmp + 1;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return trouve;
 	}
 	
 	public static void main(String[] args) {
-
+		//testQ5();
+		//testQ6();
+		//testQ7();
+		
+		//Test 2 fichiers identiques :
+		/*int premier = nextprime();
+		int finger1 = fingerprint(premier,"test1");
+		int finger2 = fingerprint(premier,"test2");
+		if(finger1 == finger2){
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		}*/
+		
+		//Test si un des fichiers contient l'autre
+		System.out.println(testContenu("test6.txt", "test7.txt"));
+		System.out.println(testContenu("test6.txt", "test8.txt"));
+		System.out.println(testContenu("test8.txt", "test7.txt"));
 	}
 }
